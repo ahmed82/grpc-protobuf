aa7dfcb88eda9a855592bbe2ad5197c95b029753
Ahmed Al Salih <ahmed82@users.noreply.github.com>
2022-02-28 15:09:26 -0500

    finish the gRPC server

--- server/server.go
+++ server/server.go
@@ -1,12 +1,41 @@
 package main
 
 import (
+	"context"
+	pb "grpc-protobuf/gen/proto"
+	"log"
+	"net"
+
 	"google.golang.org/grpc"
-	//pb "grpc-protobuf/gen/proto"
 )
 
+type testApiServer struct {
+	pb.UnimplementedTestApiServer
+}
+
+func (s *testApiServer) Echo(ctx context.Context, req *pb.ResponseRequest) (*pb.ResponseRequest, error) {
+	return req, nil
+}
+
+func (s *testApiServer) GetUser(ctx context.Context, req *pb.UserRequest) (*pb.UserResponse, error) {
+	return &pb.UserResponse{}, nil
+}
+
 func main() {
+	/// CREATE TCP SERVER
+	listner, err := net.Listen("tcp", "localhost:8080")
+	if err != nil {
+		log.Fatalln(err)
+	}
+	/// CREATE gRPC SERVER
 	grpcServer := grpc.NewServer()
 
-	pb.
+	pb.RegisterTestApiServer(grpcServer, &testApiServer{})
+
+	/// Connect gRpc & TCP SERVERS
+	err = grpcServer.Serve(listner)
+
+	if err != nil {
+		log.Println(err)
+	}
 }
